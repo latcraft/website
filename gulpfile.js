2433a174fb07d3b47cc0f09e8d02e171b24c6b09
Teodors Zeltiņš <teodors.zeltins@gmail.com>
2015-02-13 11:36:51 +0200

    added gulp build

---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+// get gulp and modules
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	sass = require('gulp-sass'),
+	bourbon = require('node-bourbon'),
+	neat = require('node-neat'),
+	autoprefix = require('gulp-autoprefixer'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	imagemin = require('gulp-imagemin'),	
+	plumber = require('gulp-plumber'),
+	concat = require('gulp-concat'),
+	jade = require('gulp-jade'),
+	clean = require('gulp-clean');
+
+// link public assets
+var publicDir = "www";
+
+// start server
+gulp.task('connect', function() {
+  connect.server({
+    root: publicDir,
+    port: 9009,
+    livereload: true
+  });
+});
+
+// minify images
+gulp.task('imagemin', function () {
+	return gulp.src(['img/*', 'img/**/*'])
+		.pipe(plumber())
+		.pipe(imagemin({progressive: true}))
+		.pipe(gulp.dest(publicDir + '/img'))
+		.pipe(connect.reload());
+});
+
+// compile sass to css and prefix
+gulp.task('sass', function () {
+	gulp.src('sass/*.scss')
+		.pipe(plumber())
+		.pipe(sass({
+	    	includePaths: neat.includePaths
+	    }))
+		.pipe(autoprefix('last 10 version'))
+		.pipe(gulp.dest(publicDir + '/css'))
+		.pipe(connect.reload());
+});
+
+// compiles jade
+gulp.task('jade', function() {
+	var news = require('./data/news.json'),
+		speaches = require('./data/speaches.json'),
+		challenges = require('./data/challenges.json');
+
+ 	gulp.src('./jade/*.jade')
+ 		.pipe(plumber())
+	    .pipe(jade({
+	    	pretty: true,
+	    	locals: {
+	    		"news": news, 	
+	    		"speaches": speaches, 	
+	    		"challenges": challenges 	
+	    	}
+	    }))
+	    .pipe(gulp.dest(publicDir))
+	    .pipe(connect.reload());
+});
+
+// clean
+gulp.task('clean', function() {
+	gulp.src(publicDir + '/*', {read: false})
+		.pipe(plumber())
+		.pipe(clean());
+});
+
+// contact js and minify
+gulp.task('uglify', function() {
+	gulp.src(['js/**/jquery.js', 'js/**/*.js', 'js/*.js'])
+		.pipe(plumber())
+		.pipe(concat('main.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(publicDir + '/js'))
+		.pipe(connect.reload());
+});
+
+// copy video
+gulp.task('video', function() {
+	gulp.src(['video/*'])
+		.pipe(plumber())
+		.pipe(gulp.dest(publicDir + '/video'))
+		.pipe(connect.reload());
+});
+
+// copy other resources
+gulp.task('copy', function() {
+	gulp.src(['favicon.ico', 'humans.txt', 'robots.txt'])
+		.pipe(gulp.dest(publicDir))
+		.pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['js/*.js', 'js/**/*.js'], ['uglify']);
+    gulp.watch(['img/*', 'img/**/*'], ['imagemin']);
+    gulp.watch(['sass/*.scss', 'sass/**/*.scss'], ['sass']);
+    gulp.watch(['jade/*.jade', 'jade/**/*.jade'], ['jade']);
+    gulp.watch(['favicon.ico', 'humans.txt', 'robots.txt'], ['copy']);
+    gulp.watch(['video/*'], ['video']);
+});
+
+
+// default task (called when you run 'gulp' from cli)
+gulp.task('build', function() {
+	gulp.start('sass', 'jade', 'uglify', 'imagemin', 'video', 'copy');
+}); 
+
+gulp.task('default', ['connect', 'build', 'watch']);
---
+++ js/main.js
@@ -0,0 +1,173 @@
+var easyPeasyParallax = function () {
+	scrollPos = $(this).scrollTop();
+
+	$('#header .info').css({
+		'margin-top': (scrollPos)+"px",
+		'opacity': 1-(scrollPos/250)
+	});
+}
+
+var easyPeasyNav = function () {
+	scrollPos = $(this).scrollTop();
+	var header = $('#header');
+	var offCanvas = $('#off-canvas');
+
+	if(scrollPos >= header.height()) {
+		offCanvas.addClass("active");
+	} else {
+		offCanvas.removeClass("active");
+	}
+}
+
+var centerArrow = function (elem) {
+	var containerWidth = $("#about .container").width();
+
+	$('#member-nav a').removeClass("active");
+	elem.addClass("active");
+
+	$('#member-list ul').removeClass("active");
+	$('#member-list ul').eq(parseInt(elem.parent().index()) - 1).addClass("active");
+
+	var itemWidth = elem.width() / 2;
+	var thisPos = elem.position();
+
+	$('#member-nav .arrow').css({
+		"left" : ((thisPos.left + itemWidth - 5)/containerWidth) * 100 + "%"
+	});	 
+}
+
+$(document).ready(function($) {
+	// fetch one tweet
+	var tweetConfig = {
+		"id": '558200865941909504',
+		"domId": 'twitter-feed',
+		"maxTweets": 3,
+		"enableLinks": true
+	};
+	twitterFetcher.fetch(tweetConfig);
+
+
+	// get flickr
+	var flickrURL = "http://api.flickr.com/services/feeds/photos_public.gne?ids=128548450@N05&format=json&jsoncallback=?";
+	$.getJSON(flickrURL, function(data){
+		var imageCollection = "";
+
+		imageCollection += '<ul>';
+		for (var item in data.items) {
+			if(item == 8) break;
+			imageCollection += '<li><img src="' + data.items[item].media.m + '" alt=""></li>';
+		}
+		imageCollection += '</ul>';
+
+		$("#flickr-feed").append(imageCollection);
+	});
+
+
+	// enable sliders, navigation and pagination
+	$('.carousel').jcarousel();
+
+	$('#news-slider .pagination ul')
+        .on('jcarouselpagination:active', 'li', function() {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'li', function() {
+            $(this).removeClass('active');
+        })
+        .jcarouselPagination({
+		    'item': function(page, carouselItems) {
+		        return '<li><a href="#' + page + '">' + page + '</a></li>';
+		    }
+		});
+
+	$('.prev').click(function() {
+	    $(this).siblings('.carousel').jcarousel('scroll', '-=1');
+
+	    return false;
+	});
+
+	$('.next').click(function() {
+	    $(this).siblings('.carousel').jcarousel('scroll', '+=1');
+
+	    return false;
+	});
+
+
+	// initialize map
+	var myLatlng = new google.maps.LatLng(56.959081,24.114304);
+	var mapOptions = {
+    	center: myLatlng,
+    	scrollwheel: false,
+    	zoom: 14
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	var marker = new google.maps.Marker({
+	    position: myLatlng,
+	    map: map,
+  		clickable: false,
+  		icon: "../img/tooltip.png",
+	    title: "Our tmp base!"
+	});
+
+
+	// open/close modal window for Eventbrite
+	var modal = $('#modal');
+	$('#get-ticket').click(function() {
+		modal.addClass("open");
+	});
+	modal.click(function() {
+		modal.removeClass("open");
+	});
+
+
+	// do parallax for top title
+	$(window).scroll(function() {
+		easyPeasyParallax();
+		if($('body').width() >= 1020) {
+			easyPeasyNav();
+		}
+	});
+
+
+	// open members
+	centerArrow($('#member-nav li:nth-child(2) a')); 
+	$('#member-nav a').click(function(e) {
+		e.preventDefault();
+
+		centerArrow($(this));    
+	});
+
+
+	// open/close off-canvas
+	$('#off-canvas a.open').click(function(e) {
+		e.preventDefault();
+
+	    $(this).parent().toggleClass('active');
+	});
+	$('#off-canvas ul a').click(function(e) {
+		var $this = $(this);
+		if($this.hasClass("twitter") || $this.hasClass("facebook") || $this.hasClass("slideshare")) return;
+
+	    var href = $(this).attr("href");
+	    $(href).animatescroll();
+
+	    if($('body').width() < 1020) {
+			$('#off-canvas').removeClass('active');
+		}
+
+	    return false;
+	});
+
+
+	// get github staff
+	$.getJSON("https://api.github.com/orgs/latcraft/members", function(data){
+		var newData = "";
+
+		for(user in data) {
+			newData += "<li><a href=\"" + data[user].html_url + "\" target=\"_blank\"><img src=\"" + data[user].avatar_url + "\" alt=\"" + data[user].login + "\"></a><div class=\"tip\">" + data[user].login + "</div></li>"
+		}
+		
+		$('#member-list ul.github').append(newData);	
+	});
+});
