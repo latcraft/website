15177d77335f02bf47f11a684fea4c8f3b741964
Ilya Alesker <ilya.alesker@ask.fm>
2016-03-31 12:34:06 +0300

    Updated run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-sudo docker run --rm -it -v ${PWD}:/app -v ${HOME}/.ssh/id_rsa:/root/.ssh/id_rsa  -p 9009:9009 -p 35729:35729 --name latcraft-website latcraft-website $@
+docker run --rm -it -v ${PWD}:/app -v ${HOME}/.ssh/id_rsa:/root/.ssh/id_rsa  -p 9009:9009 -p 35729:35729 --name latcraft-website latcraft/website $@
