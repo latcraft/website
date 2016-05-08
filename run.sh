9ae0298cb16f400b0f5c4bd8c4623c291824036a
Eduards Sizovs <eduards@sizovs.net>
2016-05-08 20:16:50 +0300

    pass GH_TOKEN to Docker container

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker run --rm -it -v ${PWD}:/app -v ${HOME}/.ssh/id_rsa:/root/.ssh/id_rsa  -p 9009:9009 -p 35729:35729 --name latcraft-website latcraft/website $@
+docker run --rm -it -v ${PWD}:/app -v ${HOME}/.ssh/id_rsa:/root/.ssh/id_rsa  -p 9009:9009 -p 35729:35729 -e GH_TOKEN=${GH_TOKEN} --name latcraft-website latcraft/website $@
